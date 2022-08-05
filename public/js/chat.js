87b8ddc94ef806ab228471e861eb163e63a29731
Bartosz Lenartowicz <bartosz.lenartowicz@nokia.com>
2022-08-05 12:06:33 +0200

    Added acknowlegmend to sending messages and sharing location && checking messages profanity

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,15 +9,27 @@ document.querySelector('#message-form').addEventListener('submit', e => {
 
     const message = e.target.elements.message.value
     
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, error => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log('The message delivered!')
+    })
 })
-
+//acknowlegmend
 document.querySelector('#send-location').addEventListener('click', () => {
+    
     if(!navigator.geolocation) {
         return alert('Geolocation is not supportet by your browser.')
     }
 
     navigator.geolocation.getCurrentPosition(position => {
-        socket.emit('sendLocation', { latitude: position.coords.latitude, longitude: position.coords.longitude })
+        socket.emit('sendLocation', { 
+            latitude: position.coords.latitude, 
+            longitude: position.coords.longitude 
+        }, 
+        () => {
+            console.log("Location shared!")
+        })
     })
 })
